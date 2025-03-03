9f80c1c6a06d6403eb9bbdd4fe695f34b506ae34
Roilion <caosonlam3003@gmail.com>
2025-03-03 07:49:37 +0700

    first commit

---
+++ src/test/java/com/example/caosonlam/CaosonlamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caosonlam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaosonlamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
